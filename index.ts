a9a03d073ad4fa28969498a6aac0eb680289b8de
Dominic Ruggiero <dominic@userexe.me>
2025-04-10 19:43:25 -0400

    mmm sockets

--- index.ts
+++ index.ts
@@ -1,14 +1,85 @@
 import env from "./env";
 
-const file = Bun.file("index.html");
+const live = Bun.file("live.html");
 const trumpet = Bun.file("trumpet.mp3");
 
+const year = 2025;
+const token = btoa(`${env.username}:${env.token}`);
+
+const divisionsLive = async () => {
+  const events = await fetch(
+    `https://frc-api.firstinspires.org/v3.0/${year}/teams/?eventCode=archimedes`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Basic ${token}`,
+      },
+    }
+  );
+  const data = await events.json();
+  return data.teamCountTotal > 0;
+};
+
+const getTeamDivision = async (teamNumber: number) => {
+  try {
+    const events = await fetch(
+      `https://frc-api.firstinspires.org/v3.0/${year}/events/?teamNumber=${teamNumber}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Basic ${token}`,
+        },
+      }
+    );
+    const data = await events.json();
+
+    const week8events = data.Events.filter(
+      (e: any) => e.type == "ChampionshipDivision"
+    );
+    return week8events.length > 0 ? week8events[0].code : null;
+  } catch (e) {
+    return null;
+  }
+};
+
+const sockets = new Map<string, Bun.ServerWebSocket<{ teamNumber: number }>>();
+setInterval(async () => {
+  console.log(`${sockets.size} sockets connected`);
+  const out = await divisionsLive();
+  if (!out) return;
+  for (const [id, ws] of sockets.entries()) {
+    const division = await getTeamDivision(ws.data.teamNumber);
+    if (division) {
+      ws.send(JSON.stringify({ division }));
+    } else {
+      ws.send(JSON.stringify({ division: "DIVISIONS ARE OUT" }));
+    }
+    ws.close();
+  }
+}, 1000);
+
 Bun.serve({
+  websocket: {
+    open(ws: Bun.ServerWebSocket<{ teamNumber: number; id: string }>) {
+      console.log("WebSocket opened");
+      sockets.set(ws.data.id, ws);
+      console.log(`WebSocket opened for team ${ws.data.teamNumber}`);
+    },
+    message(ws, message) {},
+    close(ws) {
+      sockets.delete(ws.data.id);
+      console.log("WebSocket closed");
+    },
+  },
   async fetch(request, server) {
     const url = new URL(request.url);
 
     if (url.pathname === "/") {
-      return new Response(file, {
+      return Response.redirect("/2539", 307);
+    }
+
+    if (url.pathname.match(/^\/+\d{1,5}$/)) {
+      return new Response(live, {
         headers: {
           "Content-Type": "text/html",
         },
@@ -23,25 +94,14 @@ Bun.serve({
       });
     }
 
-    if (url.pathname === "/api/data") {
-      const token = btoa(`${env.username}:${env.token}`);
-      const events = await fetch(
-        "https://frc-api.firstinspires.org/v3.0/2025/events/?teamNumber=2539",
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Basic ${token}`,
-          },
-        }
-      );
-      const data = await events.json();
-
-      const week8events = data.Events.filter((e: any) => e.weekNumber === 8);
-      return new Response(JSON.stringify(week8events), {
-        headers: {
-          "Content-Type": "application/json",
+    if (url.pathname.match(/^\/socket\/+\d{1,5}$/)) {
+      const upgraded = server.upgrade(request, {
+        data: {
+          id: Math.random().toString(36).substring(2),
+          teamNumber: parseInt(url.pathname.split("/").pop()!),
         },
       });
+      if (!upgraded) return new Response("upgrade failed", { status: 400 });
     }
 
     return new Response("404", {
